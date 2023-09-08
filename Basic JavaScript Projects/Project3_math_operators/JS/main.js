a08626f1ede82562e54f7c1e1e9b484ffff2c7e8
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 15:56:07 +0100

    Multiple Operators Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,19 +1,19 @@
 // main.js
 
-// Define a function that performs multiplication and division and displays the results
-function performMultiplicationAndDivision() {
-  // Perform the multiplication operation
-  let multiplicationResult = 5 * 6; // You can use any numbers you want here
-
-  // Perform the division operation
-  let divisionResult = 20 / 4; // You can use any numbers you want here
+// Define a function that performs multiple mathematical operations and displays the results
+function performMultipleOperations() {
+  // Perform addition, subtraction, multiplication, and division operations
+  let additionResult = 5 + 3;
+  let subtractionResult = 10 - 4;
+  let multiplicationResult = 6 * 7;
+  let divisionResult = 20 / 2;
 
   // Get the paragraph element with the id "Math"
   let mathParagraph = document.getElementById("Math");
 
   // Display the results in the paragraph
-  mathParagraph.textContent = `Result of Multiplication: ${multiplicationResult}, Result of Division: ${divisionResult}`;
+  mathParagraph.textContent = `Result of Addition: ${additionResult}, Result of Subtraction: ${subtractionResult}, Result of Multiplication: ${multiplicationResult}, Result of Division: ${divisionResult}`;
 }
 
 // Call the function when the page loads
-window.onload = performMultiplicationAndDivision;
+window.onload = performMultipleOperations;
