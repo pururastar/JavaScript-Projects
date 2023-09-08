cf8b0a6758b8f903edcb4057868fed52c8099b16
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 14:53:14 +0100

    Operator Assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,15 +1,2 @@
-// Define a function
-function changeText() {
-    // Assign two variables
-    var firstText = "Hello, ";
-    var secondText = "World!";
-    
-    // Combine the variables
-    var combinedText = firstText + secondText;
-
-    // Use document.getElementById() to get a reference to a paragraph element
-    var paragraph = document.getElementById("output");
-
-    // Change the text content of the paragraph
-    paragraph.textContent = combinedText;
-}
+// Use the += operator to concatenate a string
+document.getElementById("output").textContent += "This is a concatenated string.";
