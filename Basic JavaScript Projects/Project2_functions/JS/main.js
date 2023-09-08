ffd679619b44613ce634ec0a33a28c0ed64af43b
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 14:55:16 +0100

    Project2_Functions Assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,2 +1,12 @@
-// Use the += operator to concatenate a string
-document.getElementById("output").textContent += "This is a concatenated string.";
+// Get references to HTML elements
+let displayButton = document.getElementById("displayButton");
+let outputElement = document.getElementById("output");
+
+// Add an event listener to the button
+displayButton.addEventListener("click", function () {
+    // Call the function and store the result
+    let concatenatedString = concatenateStrings();
+
+    // Display the result in the HTML element
+    outputElement.textContent = concatenatedString;
+});
