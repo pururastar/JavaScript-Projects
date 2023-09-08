265c03992953ed25dda27c265496279d4dbd2299
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 16:06:09 +0100

    Modulus Operator Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,19 +1,18 @@
 // main.js
 
-// Define a function that performs multiple mathematical operations and displays the results
-function performMultipleOperations() {
-  // Perform addition, subtraction, multiplication, and division operations
-  let additionResult = 5 + 3;
-  let subtractionResult = 10 - 4;
-  let multiplicationResult = 6 * 7;
-  let divisionResult = 20 / 2;
+// Define a function that performs a modulus operation and displays the result
+function performModulusOperation() {
+  // Perform a modulus operation
+  let dividend = 17;
+  let divisor = 5;
+  let modulusResult = dividend % divisor;
 
   // Get the paragraph element with the id "Math"
   let mathParagraph = document.getElementById("Math");
 
-  // Display the results in the paragraph
-  mathParagraph.textContent = `Result of Addition: ${additionResult}, Result of Subtraction: ${subtractionResult}, Result of Multiplication: ${multiplicationResult}, Result of Division: ${divisionResult}`;
+  // Display the result in the paragraph
+  mathParagraph.textContent = `Result of Modulus Operation: ${modulusResult}`;
 }
 
 // Call the function when the page loads
-window.onload = performMultipleOperations;
+window.onload = performModulusOperation;
