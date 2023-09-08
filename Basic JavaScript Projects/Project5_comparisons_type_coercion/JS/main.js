20b7872a93a5f2d349a5cdc760d3017d8d64ec8a
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 16:24:40 +0100

    Type Of Operator Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,8 @@
+// Define a variable of your choice
+var myVariable = 42;
+
+// Use typeof to get the data type of the variable
+var dataType = typeof myVariable;
+
+// Display the data type in the HTML document
+document.getElementById("dataType").textContent = dataType;
