c3e9b3aee93cba5f1ea077919d1f5c556ff7bb8d
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 15:53:48 +0100

    Multiplication and Division Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,16 +1,19 @@
 // main.js
 
-// Define a function that performs subtraction and displays the result
-function performSubtraction() {
-  // Perform the subtraction operation
-  let result = 10 - 5; // You can use any numbers you want here
+// Define a function that performs multiplication and division and displays the results
+function performMultiplicationAndDivision() {
+  // Perform the multiplication operation
+  let multiplicationResult = 5 * 6; // You can use any numbers you want here
+
+  // Perform the division operation
+  let divisionResult = 20 / 4; // You can use any numbers you want here
 
   // Get the paragraph element with the id "Math"
   let mathParagraph = document.getElementById("Math");
 
-  // Display the result in the paragraph
-  mathParagraph.textContent = `Result of Subtraction: ${result}`;
+  // Display the results in the paragraph
+  mathParagraph.textContent = `Result of Multiplication: ${multiplicationResult}, Result of Division: ${divisionResult}`;
 }
 
 // Call the function when the page loads
-window.onload = performSubtraction;
+window.onload = performMultiplicationAndDivision;
