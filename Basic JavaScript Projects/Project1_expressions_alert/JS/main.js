e744d76aee3132f76471948bc421451243606740
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 14:41:05 +0100

    Comment Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,26 @@
+// This is a comment. It won't be executed by the browser.
+
+// Assign a string value to a variable.
+var greeting = "Hello, World!";
+
+// Display a pop-up alert with the greeting.
+window.alert(greeting);
+
+// Concatenate two strings and display them.
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + " " + lastName;
+document.write(fullName);
+
+// Assign multiple variables in one statement.
+var a = 5, b = 10, c = 15;
+
+// Calculate the sum of variables a and b.
+var sum = a + b;
+document.write("<br>Sum: " + sum);
+
+// Write an expression.
 var x = 5;
 var y = 10;
-var result = x + y;
+var result = x * y;
+document.write("<br>Result: " + result);
