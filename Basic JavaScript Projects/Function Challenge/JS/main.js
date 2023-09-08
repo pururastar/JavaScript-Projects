31ee6f1825e2351cc1d60dcb490e79f80e282fd2
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 15:31:49 +0100

    Function Challenge

---
+++ Basic JavaScript Projects/Function Challenge/JS/main.js
@@ -0,0 +1,7 @@
+function displayMessage() {
+    // Get the element with the ID "message"
+    var messageElement = document.getElementById("message");
+
+    // Change the content of the element
+    messageElement.innerHTML = "Hello, world!";
+}
