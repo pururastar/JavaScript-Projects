80716797b4dabffee4e530eda554f561f45044ee
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-08 15:39:26 +0100

    Math Operators Assignment

---
+++ Basic JavaScript Projects/Math Operators Assignment/JS/main.js
@@ -0,0 +1,7 @@
+function performMathOperation() {
+    // Perform a simple addition operation
+    let result = 5 + 3;
+    
+    // Display the result in the paragraph with id "Math"
+    document.getElementById("Math").innerHTML = "The result of the operation is: " + result;
+}
