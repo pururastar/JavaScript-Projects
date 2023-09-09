094c8a3d3b40b419555de2cf86e28bd12461a555
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-09 16:15:27 +0100

    NaN Challenge

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/NaN Challenge/JS/main.js
@@ -0,0 +1,8 @@
+// Display NaN
+document.write("Display NaN: " + NaN);
+
+// Display "true" with isNaN()
+document.write("<br>isNaN(NaN): " + isNaN(NaN)); // This will display "true"
+
+// Display "false" with isNaN()
+document.write("<br>isNaN(42): " + isNaN(42)); // This will display "false"
