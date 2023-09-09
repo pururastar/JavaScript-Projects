689e04e927f06bdc770d67b0cd3017f1146a49ff
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-09 16:21:13 +0100

    Boolean Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/Boolean Assignment/JS/main.js
@@ -0,0 +1,3 @@
+// Use Boolean logic to display true and false
+document.getElementById("trueResult").innerHTML = 5 > 2; // Display "true"
+document.getElementById("falseResult").innerHTML = 2 < 1; // Display "false"
