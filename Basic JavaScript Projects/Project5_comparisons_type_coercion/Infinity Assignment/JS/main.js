038da2f6f347473addb989fdddff7cdc4af87ca3
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-09 16:18:36 +0100

    Infinity Assignmente

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/Infinity Assignment/JS/main.js
@@ -0,0 +1,3 @@
+// Display Infinity and -Infinity
+document.getElementById("infinity").innerHTML = 1 / 0; // Display "Infinity"
+document.getElementById("negativeInfinity").innerHTML = -1 / 0; // Display "-Infinity"
