a2880a8a0f433754ff3e5d09c926c78c1c7ec308
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-09 16:31:04 +0100

    Double Equal Signs Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/Double Equal Signs Assignment/JS/main.js
@@ -0,0 +1,8 @@
+// Use == to perform comparisons and return "true" or "false"
+const isEqual1 = 5 == 5; // true
+const isEqual2 = 5 == '5'; // true
+const isEqual3 = 'hello' == 'world'; // false
+
+console.log(`Is 5 equal to 5?`, isEqual1);
+console.log(`Is 5 equal to '5'?`, isEqual2);
+console.log(`Is 'hello' equal to 'world'?`, isEqual3);
