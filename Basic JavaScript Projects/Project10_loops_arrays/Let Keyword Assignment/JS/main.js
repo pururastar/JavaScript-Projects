bfa4e1f0b8e9b0b466efd0d2bcde6b95d7fc64b5
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-10 10:00:47 +0100

    Let Keyword Assignment

---
+++ Basic JavaScript Projects/Project10_loops_arrays/Let Keyword Assignment/JS/main.js
@@ -0,0 +1,8 @@
+let myVariable = "This is a variable declared with let.";
+
+function showLetVariable() {
+  document.getElementById("LetKeyword").innerHTML = myVariable;
+}
+
+// Update the value of the variable (optional)
+myVariable = "The variable has been updated.";
