81690fd38a14abeeba9bbeafaa3d0b228e2a0df3
pururastar <131985208+pururastar@users.noreply.github.com>
2023-09-24 19:03:39 +0100

    One-Page Website Submission Assignment

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -1,11 +1,16 @@
-function openLightbox(element) {
-  const lightbox = document.getElementById('lightbox');
-  const lightboxImg = document.getElementById('lightbox-img');
-  lightboxImg.src = element.getAttribute('data-large');
-  lightbox.style.display = 'block';
+// Open the Lightbox
+function openLightbox(imageElement) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    lightboxImg.src = imageElement.getAttribute("data-large");
+    lightbox.style.display = "block";
 }
 
+// Close the Lightbox
 function closeLightbox() {
-  const lightbox = document.getElementById('lightbox');
-  lightbox.style.display = 'none';
+    const lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none";
 }
+
+// Ensure you have functions to navigate between images if needed
